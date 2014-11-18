e2aede37961e3e49db8724d37202b3632789ae5b
Philip Eckstrom <EckstromP@gmail.com>
2014-11-17 19:39:35 -0500

    Made profile functions more clear with explanations.

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -1,48 +1,32 @@
 App.controller('profile', function (page, profileObject) 
 {
+	//This controller works very similarly to the controller implemented by MainEvent.js. The only difference
+	//is that this version queries the database for profile-specific events. Currently, the query involved under
+	//Query.getProfileData() uses a hard-coded user (doglover) to prove successful test queries. Once an actively 
+	//logged in username is stored within the application, the hard-coded user will be replaced with the variable
+	//representing user storage.
 	
-	console.log("Do I have an object yet?:");		
-	console.log(profileObject);
+	//console.log("Object: " + profileObject + " is present");		
 
 	$(page).on('appShow', function () 
 	{
 		if(window.filterFlag==true) 
 		{
-			//alert("incoming filtered data");
 			var list = document.getElementById('profileList');
-			//clear the list, prep for new stuff
 			list.innerHTML = "";
-			
-			//DEBUG: This is where the new data is:
-			//console.log(window.eventsObject);
-			//console.log(window.orderedArray);
-			
-			//now create the list with it
 			createList(list, null)
-			//alert("finishedfilteredloading");
 		}
 
 		else 
 			
 		{
-			//alert("firsttimeloadingHOME");
-			//alert("is the flag set?:"+window.filterFlag);
-			
-			//because the flag is not set, query DB and dump everything in
 			Query.getProfileData(function(eventsData) {
 				window.profileObject = eventsData.events;
 				window.orderedArray = eventsData.order;
 				var list = document.getElementById('profileList');
-				
-				//this clears the list to fix the bug where navigating back to home duplicates the existing list
 				list.innerHTML = "";
-				
-				//dump all data into the homepage
 				createList(list, null);
 			});
-			//alert("finishedloadingfirsttime");
 		}	
-	
-		//end controller
 	});
 });		
