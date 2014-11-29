2a7800bcbf2e0bd2f28dc3308676ac4e18cd93c4
joe <jabad3.gatech.edu>
2014-11-29 18:00:51 -0500

    Fixed the bug were everything in the details page was bolded. There was no way to individualize the template from the object code. Fix: changed the page from UL to a table format.

--- public/js/EventDetails.js
+++ public/js/EventDetails.js
@@ -6,13 +6,13 @@ App.controller('details', function (page, eventObject) {
 
 			var timeOfEvent = timeOfEvent.format('h:mm A');
 
-			$(page).find('.eventName').text("Name: " + nameOfEvent);
-			$(page).find('.eventDate').text("Date: " + selectedEvent.Date);
-			$(page).find('.eventTime').text("Time: " + timeOfEvent);
-			$(page).find('.eventCategory').text("Category: " + selectedEvent.Category);
-			$(page).find('.eventDescription').text("Description: " + selectedEvent.Description);
-			$(page).find('.eventLocation').text("Location: " + selectedEvent.Location);
-			$(page).find('.eventHost').text("Host: " + selectedEvent.Host);
+			$(page).find('.eventName').text(nameOfEvent);
+			$(page).find('.eventDate').text(selectedEvent.Date);
+			$(page).find('.eventTime').text(timeOfEvent);
+			$(page).find('.eventCategory').text(selectedEvent.Category);
+			$(page).find('.eventDescription').text(selectedEvent.Description);
+			$(page).find('.eventLocation').text(selectedEvent.Location);
+			$(page).find('.eventHost').text(selectedEvent.Host);
 
 			
 
