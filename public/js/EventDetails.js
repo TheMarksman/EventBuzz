f2f1d4b3d5548e5ce81f0dee27a88221d1753cfc
TheMarksman <tmm140@gmail.com>
2014-11-30 22:26:46 -0500

    trying to get rsvp working

--- public/js/EventDetails.js
+++ public/js/EventDetails.js
@@ -1,60 +1,26 @@
 App.controller('details', function (page, eventObject) {
-			//put stuff here
-			var selectedEvent = eventObject.event;
-			var nameOfEvent = eventObject.eventName;
-			var timeOfEvent = moment(selectedEvent.Date + ', ' + selectedEvent.Time);
+	if (window.username) {
+		$('.rsvp-button').css('display', 'inline');
+		$('.rsvp-button').css('display', 'inline');
+	} else {
+		$('.rsvp-button').css('display', 'none');
+	}
+	var selectedEvent = eventObject.event;
+	var nameOfEvent = eventObject.eventName;
+	var timeOfEvent = moment(selectedEvent.Date + ', ' + selectedEvent.Time);
 
-			var timeOfEvent = timeOfEvent.format('h:mm A');
+	var timeOfEvent = timeOfEvent.format('h:mm A');
 
-			$(page).find('.eventName').text(nameOfEvent);
-			$(page).find('.eventDate').text(selectedEvent.Date);
-			$(page).find('.eventTime').text(timeOfEvent);
-			$(page).find('.eventCategory').text(selectedEvent.Category);
-			$(page).find('.eventDescription').text(selectedEvent.Description);
-			$(page).find('.eventLocation').text(selectedEvent.Location);
-			$(page).find('.eventHost').text(selectedEvent.Host);
+	$(page).find('.eventName').text(nameOfEvent);
+	$(page).find('.eventDate').text(selectedEvent.Date);
+	$(page).find('.eventTime').text(timeOfEvent);
+	$(page).find('.eventCategory').text(selectedEvent.Category);
+	$(page).find('.eventDescription').text(selectedEvent.Description);
+	$(page).find('.eventLocation').text(selectedEvent.Location);
+	$(page).find('.eventHost').text(selectedEvent.Host);
 
-			
-
-			/*
-			var catcount = 0;
-			if(eventObject.event.Category == "Sports"){
-				$(page).find('.eventCategory').text("Category: #Sports");
-				catcount++;
-			}
-			if(eventObject.event.Category = ){
-				if(catcount == 1){
-					$(page).find('.eventCategory2').text("#Clergy")
-
-				}
-				else{
-					$(page).find('.eventCategory').text("#Clergy")
-				}
-			}
-			if(eventObject.event.categories["infosession"]){
-					if(catcount==1){
-						$(page).find('.eventCategory2').text("#Infosession")
-
-					}
-					if(catcount==2){
-						$(page).find('.eventCategory3').text("#Infosession")
-					}
-				else{
-					$(page).find('.eventCategory').text("#Clergy")
-				}
-				$(page).find('.eventCategory').text("Info Session")
-			}
-			if(eventObject.event.categories["atl"]){
-				$(page).find('.eventCategory').text("#Atlanta")
-			}
-			if(eventObject.event.categories["coc"]){
-				$(page).find('.eventCategory').text("#College of Computing")
-			}
-			if(eventObject.event.categories["greek"]){
-				$(page).find('.eventCategory').text("#Greek")
-			}
-			*/
 });
+
 try {
 	App.restore();
 } catch (err) {
